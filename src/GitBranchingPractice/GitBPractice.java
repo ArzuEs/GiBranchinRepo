d8c0044ac45b58c0abdb56e898e9011ef51ba058
arzu <arzu>
2022-11-09 11:47:44 -0500

    this is belongs to Git Branching Practice

---
+++ src/GitBranchingPractice/GitBPractice.java
@@ -0,0 +1,13 @@
+package GitBranchingPractice;
+
+public class GitBPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+			System.out.println("this is Git Branching Practice");
+		}
+		
+	}
+
+
